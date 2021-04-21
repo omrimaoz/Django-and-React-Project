6acb99b01b9244e09192a536bd40420929b09c80
Omri Maoz <75577504+omrimaoz@users.noreply.github.com>
2021-04-22 00:53:44 +0300

    Django and React Project - 5'th commit
    CreateNewRoomPage.js UI

--- music_controller/frontend/src/components/RoomJoinPage.js
+++ music_controller/frontend/src/components/RoomJoinPage.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 
+
 export default class RoomJoinPage extends Component {
     constructor(props) {
         super(props);
     }
     render(){
-        return <p> This is the Room Join Page </p>;
+        return(<p> Thi is the Join Room</p>
+    );
     }
 }
